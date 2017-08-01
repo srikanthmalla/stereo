7f9afe534119ef929a179188d03bdffddecd4c8f
srikanthmalla <mallasrikanth004@gmail.com>
2017-07-31 22:55:24 -0400

    rectification with epipolar lines

--- euromav/left_cam.py
+++ euromav/left_cam.py
@@ -8,18 +8,21 @@ import numpy as np
 #   cols: 4
 #   rows: 4
 
-T=np.array( [[0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975],
+transformation=np.array( [[0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975],
          		[0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768],
        			[-0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949],
-         		[0.0, 0.0, 0.0, 1.0]],dtype=np.float32)
+         		[0.0, 0.0, 0.0, 1.0]],dtype=np.float64)
 
 # Camera specific definitions.
 # rate_hz: 20
-resolution=np.array([752, 480],dtype=np.float32)
+resolution=np.array([752, 480],dtype=np.float64)
 camera_model= 'pinhole'
-intrinsics=np.array([458.654, 457.296, 367.215, 248.375],dtype=np.float32) #fu, fv, cu, cv
+intrinsics=np.array([458.654, 457.296, 367.215, 248.375],dtype=np.float64) #fu, fv, cu, cv
+camera_matrix=np.array([[intrinsics[0],0,intrinsics[2]],
+						[0,intrinsics[1],intrinsics[3]],
+						[0,0,1]],dtype=np.float64)
 
 # distortion_coefficients
-k=np.array([-0.28340811, 0.07395907, 0.00019359, 1.76187114e-05],dtype=np.float32)
+dist_coeffs=np.array([-0.28340811, 0.07395907, 0.00019359, 1.76187114e-05],dtype=np.float64)
 distortion_model= 'radial-tangential'
 
--- main.py
+++ main.py
@@ -2,13 +2,33 @@ import cv2
 import numpy as np
 import euromav.left_cam as L
 import euromav.right_cam as R
-i_l=cv2.imread('./euromav/left.png')
-i_r=cv2.imread('./euromav/right.png')
+from draw_epipolarlines import * 
 
-while (True):	
-    cv2.imshow('Left Image',i_l)
-    cv2.imshow('Right Image',i_r)
+left_frame=cv2.imread('./euromav/left.png')
+right_frame=cv2.imread('./euromav/right.png')
+
+h, w = left_frame.shape[:2] # both frames should be of same shape
+
+#transformation between two cameras
+T=np.matmul(np.linalg.inv(R.transformation),L.transformation)
+rotation=T[0:3,0:3]
+translation=T[0:3,3]
+
+#Perform stereorectification 
+R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(L.camera_matrix, L.dist_coeffs, R.camera_matrix, R.dist_coeffs, (w,h), rotation, translation, cv2.CALIB_ZERO_DISPARITY,0, (0,0))
+mapxL, mapyL = cv2.initUndistortRectifyMap(L.camera_matrix, L.dist_coeffs, R1, P1, (w,h), cv2.CV_32FC1)
+mapxR, mapyR = cv2.initUndistortRectifyMap(R.camera_matrix, R.dist_coeffs, R2, P2, (w,h), cv2.CV_32FC1)
+dstL = cv2.remap(left_frame, mapxL, mapyL,cv2.INTER_LINEAR)
+dstR = cv2.remap(right_frame, mapxR, mapyR,cv2.INTER_LINEAR)
+
+draw_epipolarlines(left_frame,right_frame)
+draw_epipolarlines(dstL,dstR)
+
+# display the images
+# while (True):	
+    # cv2.imshow('Left Image',img3)
+    # cv2.imshow('Right Image',img5)
     # cv2.imshow('Left rectify',dstL)
     # cv2.imshow('Right rectify',dstR)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
